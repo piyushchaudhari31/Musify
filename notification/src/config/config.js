1bab6a58350f44c5e55b61661b7219b8a27b755d
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2025-11-22 12:56:59 +0530

    delete notification folder

--- notification/src/config/config.js
+++
@@ -1,7 +0,0 @@
-import { config as dotenvConfig } from "dotenv";
-dotenvConfig();
-
-export default {
-    RABBITMQ_URI: process.env.RABBITMQ_URI,
-    RESEND_API_KEY: process.env.RESEND_API_KEY
-};
