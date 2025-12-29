7614bf4d5f4928952feb7dee56772139810ad3b5
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2025-12-29 18:02:08 +0530

    Update ArtistSong.jsx

--- frontend/src/pages/artist/ArtistSong.jsx
+++ frontend/src/pages/artist/ArtistSong.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
-import "./ArtistSong.css";
+import "./artistsong.css";
 
 const ArtistSong = () => {
 
